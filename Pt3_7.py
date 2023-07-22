3861a8f4a85e5e6925f1c59db7087849873d03a3
d3aikina <138819561+d3aikina@users.noreply.github.com>
2023-07-22 11:41:04 +0700

    Pt3_7: Vowels and consonants
    Program that creates a dictionary where the keys are letters from a string, and the values are True if the letter is a vowel, and False if the letter is a consonant.

--- Pt3_7.py
+++ Pt3_7.py
@@ -1,1 +1,1 @@
-print({x:(lambda a: True if a in 'аеёиоуыэюяaeiou' else False)(x) for x in sorted(list(set(input('Введите строку: ').lower().replace(' ','')))) if x.isalpha()})
+print({x: (lambda a: True if a in 'аеёиоуыэюяaeiou' else False)(x) for x in sorted(list(set(input('Введите строку: ').lower().replace(' ','')))) if x.isalpha()})
