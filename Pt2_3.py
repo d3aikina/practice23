bb85661e6c8c10858bfb420fd558ff07b108c349
d3aikina <138819561+d3aikina@users.noreply.github.com>
2023-07-22 11:14:26 +0700

    Pt2_3: Armstrong's number
    Program that checks whether a number is an Armstrong number.

--- Pt2_3.py
+++ Pt2_3.py
@@ -1,5 +1,5 @@
 n = input('Введите число: ')
-if (sum([int(x)**len(n) for x in n]) == int(n)):
+if (sum([int(i)**len(n) for i in n]) == int(n)):
   print(f'{n} является числом Армстронга')
 else:
     print(f'{n} не является числом Армстронга')
