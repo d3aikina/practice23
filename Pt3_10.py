3e3953ad9129d874ea6b44ac6e0270995de67e94
d3aikina <138819561+d3aikina@users.noreply.github.com>
2023-07-22 11:30:40 +0700

    Pt3_10: Letters count
    Program that creates a dictionary containing only letters per line and their count, ignoring spaces.

--- Pt3_10.py
+++ Pt3_10.py
@@ -1,3 +1,3 @@
-s = [x for x in input('Введите строку: ').replace(' ','') if x.isalpha()]
+s = [a for a in input('Введите строку: ').replace(' ','') if a.isalpha()]
 dict = {sorted(list(set(s)))[i]:s.count(sorted(list(set(s)))[i]) for i in range(len(set(s)))}
 print(dict)
