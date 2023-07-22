1c0379f31c25cfc2cee925260bdd696377fa71e4
d3aikina <138819561+d3aikina@users.noreply.github.com>
2023-07-22 11:32:03 +0700

    Pt3_11: Unique letters
    Program that creates a dictionary containing only unique letters in line and their count, ignoring spaces.

--- Pt3_11.py
+++ Pt3_11.py
@@ -1,3 +1,3 @@
-s = [x for x in input('Введите строку: ').replace(' ','') if x.isalpha()]
+s = [a for a in input('Введите строку: ').replace(' ','') if a.isalpha()]
 dict = {sorted(list(set(s)))[i]:1 for i in range(len(set(s))) if (s.count(sorted(list(set(s)))[i]) == 1) }
 print(dict)
