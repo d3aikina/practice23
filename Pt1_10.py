dbdd6d234495e8ec749edf13468080d018120620
d3aikina <138819561+d3aikina@users.noreply.github.com>
2023-07-22 12:10:26 +0700

    Pt1_10: Float formatting
    Program that asks user for a float number and returns it in a formatted way.

--- Pt1_10.py
+++ Pt1_10.py
@@ -1,9 +1,9 @@
 x = round(float(input('Введите вещественное число: ')), 2)
 s = '.' + str(x).split('.')[1]
-s = s + '0'*(3-len(s))
+s = s + '0' * (3 - len(s))
 x = int(str(x).split('.')[0])
-while x//1000>0:
-  s = ',' + '0'*(3-len(str(x%1000))) + str(x%1000) + s
-  x//=1000
-s = str(x)+s
+while x // 1000>0:
+    s = ',' + '0' * (3-len(str(x%1000))) + str(x % 1000) + s
+    x //= 1000
+s = str(x) + s
 print(s)
